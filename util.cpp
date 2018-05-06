a628899dc75480e810bb3c544ca65d92601003fc
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-05-06 16:54:12 +0430

    extract_int + read_weights_and_bias + read_inputs functions

--- util.cpp
+++ util.cpp
@@ -1,7 +1,7 @@
 #include "util.h"
 
 void log(string txt) {
-  cerr << txt << endl;
+  cout << txt << endl;
 }
 
 pthread_t create_thread(void *start_routine, void *arg) {
@@ -9,4 +9,55 @@ pthread_t create_thread(void *start_routine, void *arg) {
   if (pthread_create(&thread, NULL, start_routine, arg) != 0)
     log("ERR! pthread_create!");
   return thread;
+}
+
+vector<vector<int> > read_inputs() {
+  ifstream file("./inputs.txt");
+  vector<vector<int> > res;
+  if (file.is_open()) {
+    string word;
+    while (file >> word) {
+      if (word.find(",") != -1) {
+        word.erase(remove(word.begin(), word.end(), '{'), word.end());
+        word.erase(remove(word.begin(), word.end(), '}'), word.end());
+        res.push_back(extract_int(word, ","));
+      }
+    }
+  }
+  return res;
+}
+
+vector<int> read_weights_and_bias() {
+  ifstream file("./weights.txt");
+  vector<int> res;
+  bool is_bias = false;
+  if (file.is_open()) {
+    string word;
+    while (file >> word) {
+      if (word.find(",") != -1) {
+        word.erase(remove(word.begin(), word.end(), '{'), word.end());
+        word.erase(remove(word.begin(), word.end(), '}'), word.end());
+        res = extract_int(word, ",");
+      }
+      if (is_bias)
+        res.push_back(atoi(word.c_str()));
+      if (word.find("Bias") != -1)
+        is_bias = true;
+    }
+  }
+  return res;
+}
+
+vector<int> extract_int(string str, string delimiter) {
+  vector<int> res;
+  if (str.empty())
+    return res;
+  int pos;
+  while ((pos = str.find(delimiter)) != -1) {
+    string token = str.substr(0, pos);
+    str.erase(0, pos + delimiter.length());
+    res.push_back(atoi(token.c_str()));
+  }
+  res.push_back(atoi(str.c_str()));
+  return res;
 }
--- util.h
+++ util.h
@@ -2,5 +2,11 @@
 #include <pthread.h>
 using namespace std;
 
+#define PI 3.14159265
+
+
 void log(string txt);
 pthread_t create_thread(void *start_routine, void *arg);
+vector<int> extract_int(string str, string delimiter);
+vector<vector<int> > read_inputs();
+vector<int> read_weights_and_bias();
